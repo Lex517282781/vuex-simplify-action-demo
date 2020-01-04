7c01d3c34ba884f4d18418e7e01de31dc529499e
517282781 <517282781@qq.com>
2020-01-04 17:57:40 +0800

    actionsEnhance update

--- src/views/home/store.js
+++ src/views/home/store.js
@@ -1,13 +1,46 @@
+import service from '@/service';
+import actionsEnhance from 'vuex-simplify-action';
+
 const state = {};
 const getters = {};
 const mutations = {};
-const actions = {};
+const actions = {
+  async getfoo() {
+    const res = await service.getfoo();
+    if (!res) return false;
+
+    return res;
+  },
+  async getbaz() {
+    const res = await service.getbaz();
+    if (!res) return false;
+
+    return res;
+  },
+  async getbar({ dispatch }) {
+    const res = await service.getbar();
+    if (!res) return false;
+
+    const next = function() {
+      dispatch('getbaz');
+    };
+    return {
+      res,
+      next
+    };
+  }
+};
 
 const store = {
   namespaced: true,
   state,
   getters,
   mutations,
+  @actionsEnhance([
+    { name: 'foo', action: 'getfoo' },
+    { name: 'bar', action: 'getbar' },
+    { name: 'baz', action: 'getbaz' }
+  ])
   actions
 };
 
